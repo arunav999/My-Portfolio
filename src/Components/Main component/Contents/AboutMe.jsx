0ea1d8f91d6205a79af763487dff1eb709c41a4c
Arunav Singh <arunavsingh999@gmail.com>
2024-11-24 23:44:18 +0530

    renamed css file of about me

--- src/Components/Main component/Contents/AboutMe.jsx
+++ src/Components/Main component/Contents/AboutMe.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 
-import classes from "./AboutMe.module.css";
+import "./AboutMe-module.css";
 
 const AboutMe = () => {
   return (
-    <>
-      <section>
-        <p className={classes.myName}>About Me Section</p>
-      </section>
-    </>
+    <section className="aboutMeSection">
+      <h2 className="heading">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="headIcon"
+          viewBox="0 0 448 512"
+        >
+          <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
+        </svg>
+        About <span>Me</span>
+      </h2>
+
+      <div className="">
+        <div className="image"></div>
+
+        <div className="content"></div>
+      </div>
+    </section>
   );
 };
 
