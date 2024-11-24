be9b8d5e7109476aa343d1e7bfe6da83d9a9d02f
Arunav Singh <arunavsingh999@gmail.com>
2024-11-25 00:45:54 +0530

    about me contnent completed
    desgin remaining

--- src/Components/Main component/Contents/AboutMe.jsx
+++ src/Components/Main component/Contents/AboutMe.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 import "./AboutMe-module.css";
+import WorkingNB from "../../../My Memoji/Without background/Working-NB.png";
 
 const AboutMe = () => {
+  const data = {
+    myName: "Arunav Singh",
+    occupation: "Web Developer",
+    description:
+      "Aspiring Web Developer and Web Designer with a focus on front-end development. Proficient in HTML, CSS, JavaScript, and ReactJS at a beginner level, with a passion for creating responsive and user-friendly web interfaces. Dedicated to learning and improving in the field of web development.",
+    email: "arunavsingh999@gmail.com",
+    phone: "+91-7008122921",
+    place: "Bengaluru, India - 560048",
+  };
+
   return (
     <section className="aboutMeSection">
-      <h2 className="heading">
+      <h2 className="heading-secondary">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="headIcon"
@@ -16,10 +27,44 @@ const AboutMe = () => {
         About <span>Me</span>
       </h2>
 
-      <div className="">
-        <div className="image"></div>
+      <div className="row">
+        <div className="image">
+          <img src={WorkingNB} alt="my-image" className="memoji-working" />
+        </div>
 
-        <div className="content"></div>
+        <div className="content">
+          <h3 className="heading-tertiary">{data.myName}</h3>
+          <span>{data.occupation}</span>
+          <p className="desc">{data.description}</p>
+
+          <div className="box-container">
+            <p className="email">
+              <a
+                href="mailto:arunavsingh999@gmail.com"
+                className="contact-link"
+              >
+                <span>E-mail: </span>
+                {data.email}
+              </a>
+            </p>
+            <p className="phone">
+              <a href="tel:7008122921" className="contact-link">
+                <span>Phone: </span>
+                {data.phone}
+              </a>
+            </p>
+            <p className="place">
+              <span>Place: </span>
+              {data.place}
+            </p>
+          </div>
+
+          <div className="res-btn">
+            <a href="" className="res-link" target="blank">
+              Resume
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
