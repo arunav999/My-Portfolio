b7bf4ce4f51779d3104b4bde4ae950505644b705
Arunav Singh <arunavsingh999@gmail.com>
2024-12-06 20:50:54 +0530

    completed contact me form logic and added server code in vite.js

--- src/Components/Main component/Contents/ContactMe.jsx
+++ src/Components/Main component/Contents/ContactMe.jsx
@@ -55,6 +55,12 @@ const ContactMe = () => {
 
     const newErrors = { ...errors };
 
+    if (!value.trim()) {
+      delete newErrors[name];
+      setErrors(newErrors);
+      return;
+    }
+
     if (name === "name" && (!value.trim() || value.length < 3)) {
       newErrors.name = "Name must be at least 3 characters long";
     } else if (
@@ -106,15 +112,20 @@ const ContactMe = () => {
         ]),
       });
 
-      await response.json();
-      setData({
-        ...data,
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
-      });
-      setErrors({});
+      if (response.ok) {
+        await response.json();
+        setData({
+          ...data,
+          name: "",
+          email: "",
+          phone: "",
+          message: "",
+        });
+        setErrors({});
+        alert("Message sent successfully!!!");
+      } else {
+        alert("Submission Failed!!!");
+      }
     } catch (error) {
       console.log("Submission failed:", error);
     }
@@ -150,10 +161,8 @@ const ContactMe = () => {
                     name="name"
                     value={data.name}
                     onChange={handleChange}
-                    required
                     autoComplete="off"
                   />
-                  {errors.name && <span className="error">{errors.name}</span>}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="form-icon"
@@ -161,6 +170,9 @@ const ContactMe = () => {
                   >
                     <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
                   </svg>
+                  <div className="error">
+                    {errors.name && <p>&times; {errors.name}</p>}
+                  </div>
                 </div>
                 <div className="field">
                   <input
@@ -169,12 +181,9 @@ const ContactMe = () => {
                     name="email"
                     value={data.email}
                     onChange={handleChange}
-                    required
                     autoComplete="off"
                   />
-                  {errors.email && (
-                    <span className="error">{errors.email}</span>
-                  )}
+
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="form-icon"
@@ -182,6 +191,9 @@ const ContactMe = () => {
                   >
                     <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
                   </svg>
+                  <div className="error">
+                    {errors.email && <p>&times; {errors.email}</p>}
+                  </div>
                 </div>
                 <div className="field">
                   <input
@@ -190,12 +202,9 @@ const ContactMe = () => {
                     name="phone"
                     value={data.phone}
                     onChange={handleChange}
-                    required
                     autoComplete="off"
                   />
-                  {errors.phone && (
-                    <span className="error">{errors.phone}</span>
-                  )}
+
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="form-icon"
@@ -203,18 +212,18 @@ const ContactMe = () => {
                   >
                     <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
                   </svg>
+                  <div className="error">
+                    {errors.phone && <p>&times; {errors.phone}</p>}
+                  </div>
                 </div>
                 <div className="message">
                   <textarea
                     name="message"
                     value={data.message}
                     onChange={handleChange}
-                    required
                     placeholder="Message"
                   ></textarea>
-                  {errors.message && (
-                    <span className="error">{errors.message}</span>
-                  )}
+
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="form-icon"
@@ -222,14 +231,14 @@ const ContactMe = () => {
                   >
                     <path d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z" />
                   </svg>
+                  <div className="error">
+                    {errors.message && <p>&times; {errors.message}</p>}
+                  </div>
                 </div>
               </div>
 
               <div className="button-area">
-                <button type="submit">
-                  Submit
-                  <i></i>
-                </button>
+                <button type="submit">Submit</button>
               </div>
             </form>
           </div>
